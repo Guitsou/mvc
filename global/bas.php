9b682715f02160bdca6b0950cfa2d80f54ee1d6d
Guitsou <guillaume@reynaud.biz>
2014-12-17 10:17:39 +0100

    Mise en place des fichiers

---
+++ global/bas.php
@@ -0,0 +1,9 @@
+</div>
+
+<div id="bas">
+	Tutoriel sur le MVC
+</div>
+
+</body>
+
+</html>
---
+++ global/menu.php
@@ -0,0 +1,8 @@
+<div id="menu">
+	<h2>Menu</h2>
+
+	<ul>
+		<li><a href="index.php">Accueil</a></li>
+	</ul>
+
+</div>
